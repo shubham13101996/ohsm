cf23815e4f8929ff4b2639d51459705edd4df968
shubhampathak <shubham.pathak926@gmail.com>
2023-12-12 17:07:27 +0530

    readme add

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -32,7 +32,7 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">
-              🚪 METAVERSE LOGIN
+              🚪 OHSM LOGIN
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,35 +1,166 @@
-import React from "react";
-import Layouts from "../components/Layout/Layouts";
-import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  max-width: 400px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h2`
+  color: #ff69b4; /* Pink */
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  margin-bottom: 10px;
+`;
+
+const Input = styled.input`
+  padding: 8px;
+  margin-bottom: 15px;
+  border: 1px solid #ff69b4; /* Pink */
+  border-radius: 4px;
+`;
+
+const Button = styled.button`
+  background-color: #ff69b4; /* Pink */
+  color: #fff;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e05595; /* Lighter pink on hover */
+  }
+`;
 
 const Welcome = () => {
+  const [property, setProperty] = useState({
+    propertyType: '',
+    propertyName: '',
+    phone: '',
+    email: '',
+    address:'',
+    state:'',
+    city:'',
+    pincode:'',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProperty((prevProperty) => ({ ...prevProperty, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can add your logic here to handle the form submission
+    console.log('Submitted:', property);
+  };
+
   return (
-    <Layouts title={"Wecome to Metaverse"}>
-      <div className="row display ">
-        <div className="col-md-12 ">
-          <img
-            src="https://i.ytimg.com/vi/eJG0hhPafsI/maxresdefault.jpg"
-            style={{ width: "100%" }}
+    <Container>
+      <Title>Property Setup Form</Title>
+      <Form onSubmit={handleSubmit}>
+        <Label>
+          Property Type:
+          <br/>
+          <Input
+            type="text"
+            name="propertyType"
+            value={property.propertyType}
+            onChange={handleChange}
           />
-        </div>
-        <div className="col-md-4">
-          <h1 className="bg-dark p-2 text-white text-center">JOIN US</h1>
-          <p className="text-justify mt-2">
-            any query and info about product feel free to call us anytime, we
-            are 24 X 7 available
-          </p>
-          <p className="mt-3">
-            <BiMailSend /> : www.help@ecommerceapp.com
-          </p>
-          <p className="mt-3">
-            <BiPhoneCall /> : 012-3456789
-          </p>
-          <p className="mt-3">
-            <BiSupport /> : 1800-0000-0000 (toll free)
-          </p>
-        </div>
-      </div>
-    </Layouts>
+        </Label>
+
+        <Label>
+          Property Name :
+          <br/>
+          <Input
+            type="text"
+            name="propertyName"
+            value={property.propertyName}
+            onChange={handleChange}
+          />
+        </Label>
+
+        <Label>
+          Phone Number :
+          <br/>
+          <Input
+            type="number"
+            name="phone"
+            value={property.phone}
+            onChange={handleChange}
+          />
+        </Label>
+
+        <Label>
+          Email Address:
+          <br/>
+          <Input
+            type="email"
+            name="email"
+            value={property.email}
+            onChange={handleChange}
+          />
+        </Label>
+
+        <Label>
+           Address :
+           <br/>
+          <Input
+            type="text"
+            name="address"
+            value={property.address}
+            onChange={handleChange}
+          />
+        </Label>
+
+        <Label>
+          State :
+          <br/>
+          <Input
+            type="text"
+            name="state"
+            value={property.state}
+            onChange={handleChange}
+          />
+        </Label>
+
+
+        <Label>
+          City :
+          <br/>
+          <Input
+            type="type"
+            name="city"
+            value={property.city}
+            onChange={handleChange}
+          />
+        </Label>
+
+        <Label>
+          Pincode :
+          <br/>
+          <Input
+            type="text"
+            name="pincode"
+            value={property.pincode}
+            onChange={handleChange}
+          />
+        </Label>
+        <Button type="submit">Complete Your Setup</Button>
+      </Form>
+    </Container>
   );
 };
 
